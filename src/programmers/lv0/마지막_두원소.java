5cdc72b84c1037f6e8853bdc59e92e0a2de9b18b
changwun <feelsogood7028@naver.com>
2026-01-23 17:02:23 +0900

    fix: 리스트로 담아서 반환해 보기(마지막 두원소)

--- src/programmers/lv0/마지막_두원소.java
+++ src/programmers/lv0/마지막_두원소.java
@@ -1,28 +1,47 @@
 package programmers.lv0;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class 마지막_두원소 {
   public int[] solution(int[] num_list) {
-    int[] answer = new int[num_list.length + 1];
-    int last = num_list[num_list.length -1];
-    int lastBefore = num_list[num_list.length -2];
+//    int[] answer = new int[num_list.length + 1];
+//    int last = num_list[num_list.length -1];
+//    int lastBefore = num_list[num_list.length -2];
+//
+//    for(int i=0; i < num_list.length; i++){
+//      answer[i] = num_list[i];
+//    }
+//    if(last > lastBefore){
+//      answer[num_list.length] = last - lastBefore;
+//    }else{
+//      answer[num_list.length] = last * 2;
+//    }
+//    return answer;
+//  }
+    List<Integer> answer = new ArrayList<>();
+    int last = num_list[num_list.length - 1];
+    int lastBefore = num_list[num_list.length - 2];
 
-    for(int i=0; i < num_list.length; i++){
-      answer[i] = num_list[i];
+    for (int i = 0; i < num_list.length; i++) {
+      answer.add(num_list[i]);
     }
-    if(last > lastBefore){
-      answer[num_list.length] = last - lastBefore;
-    }else{
-      answer[num_list.length] = last * 2;
+    if (last > lastBefore) {
+      answer.add(last - lastBefore);
+    } else {
+      answer.add(last * 2);
     }
-    return answer;
+    int[] result = new int[answer.size()];
+    for (int i = 0; i < answer.size(); i++) {
+      result[i] = answer.get(i);
+    }
+    return result;
   }
-
   public static void main(String[] args) {
     마지막_두원소 sol = new 마지막_두원소();
-    int[] result = {2,1,6};
-    System.out.println(Arrays.toString(sol.solution(result)));
+    int[] input = {2,1,6};
+    System.out.println(Arrays.toString(sol.solution(input)));
   }
 
 }
