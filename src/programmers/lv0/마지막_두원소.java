94b69b45898af3681b3fbfd03bf1c5816dce56ef
changwun <feelsogood7028@naver.com>
2026-01-23 16:51:00 +0900

    fix:마지막 두원소 간결화

--- src/programmers/lv0/마지막_두원소.java
+++ src/programmers/lv0/마지막_두원소.java
@@ -5,13 +5,16 @@ import java.util.Arrays;
 public class 마지막_두원소 {
   public int[] solution(int[] num_list) {
     int[] answer = new int[num_list.length + 1];
+    int last = num_list[num_list.length -1];
+    int lastBefore = num_list[num_list.length -2];
+
     for(int i=0; i < num_list.length; i++){
       answer[i] = num_list[i];
     }
-    if(num_list[num_list.length - 1] > num_list[num_list.length - 2]){
-      answer[num_list.length] = num_list[num_list.length - 1] - num_list[num_list.length - 2];
+    if(last > lastBefore){
+      answer[num_list.length] = last - lastBefore;
     }else{
-      answer[num_list.length] = num_list[num_list.length - 1] * 2;
+      answer[num_list.length] = last * 2;
     }
     return answer;
   }
